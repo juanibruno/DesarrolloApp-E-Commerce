d2b72af040e6fb14beac52fd76373fb099e40b1a
juan ignacio bruno <juanbruno88@hotmail.com>
2024-02-11 19:45:37 -0300

    correccion 2da preEntrega

--- src/components/CardCategories.js
+++ src/components/CardCategories.js
@@ -1,5 +1,5 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native'
-import ShadowPrimary from './wrappers/ShadowPrimary'
+import ShadowPrimary from './Wrappers/ShadowPrimary'
 import Colors from '../utils/globals/Colors'
 
 
