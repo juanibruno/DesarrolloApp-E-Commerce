f25a5711cab5d810ed8d6908a620e8cc44d6c7cb
juan ignacio bruno <juanbruno88@hotmail.com>
2024-02-25 16:46:54 -0300

    nueva pre-entrega

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -11,10 +11,21 @@ export const cartSlice = createSlice({
     name: "cart",
     initialState,
     reducers: {
-        addCArtItem: (state, action) => {
-            state.items = [...state.items, {...action.payload, quantity:1}]
+        addCArtItem: (state, actions) => {
+            const index = state.items.findIndex((item) => item.id === actions.payload.id)
+            if (index === -1) {
+                state.items = [...state.items, { ...actions.payload, quantity: 1 }]
+            } else {
+                state.items = state.items.map((item) => {
+                    if (item.id === actions.payload.id) {
+                        return { ...item, quantity: item.quantity + 1 }
+                    }
+                    return item
+                })
+            }
+
             state.total = state.items.reduce((acc, item) => acc = acc + item.price, 0)
-            console.log(state);
+            
 
         },
         deleteCartItem: (state, actions) => {
