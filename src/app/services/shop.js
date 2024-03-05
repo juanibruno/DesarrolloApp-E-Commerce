5d190f84c3801cc33fe5bbe16c9e39b5fdd835d0
juan ignacio bruno <juanbruno88@hotmail.com>
2024-03-05 19:08:27 -0300

    todavia sin mejorar el error

--- src/app/services/shop.js
+++ src/app/services/shop.js
@@ -1,11 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+console.log(createApi)
+
 export const shopApi = createApi ({
+
     reducerPath:"shopApi",
-    baseQuery:fetchBaseQuery({baseUrl:"https://desarrollo-app-8eab9-default-rtdb.firebaseio.com"}),
+    baseQuery:fetchBaseQuery({baseUrl:"https://desarrollo-app-8eab9-default-rtdb.firebaseio.com/"}),
     endpoints:(builder) => ({
         getProductsByCategory: builder.query({
-            query: (category) => `/products.json?orderBy="category"&equalTo="${category}"`
+            query: (category) => `/products.json?orderBy="category"&equalTo="${category}"`,
+            transformResponse:(response) =>{
+                const data = Object.values(response)
+                return data
+            } 
         }),
         getCategories:builder.query({
             query: () => "/categories.json"
--- src/screens/ProductsByCategory.js
+++ src/screens/ProductsByCategory.js
@@ -1,28 +1,29 @@
 import { StyleSheet, FlatList } from 'react-native'
-import products from '../utils/data/products.json'
 import { useEffect, useState } from 'react'
 import ProductoPorCategoria from '../components/ProductoPorCategoria'
 import Search from '../components/Search'
+import { useGetProductsByCategoryQuery } from '../app/services/shop'
 
 
 const ProductsByCategory = ({ navigation, route }) => {
 
-  const {categorySelected} = route.params
+  const { categorySelected } = route.params
+  const { data: products } = useGetProductsByCategoryQuery(categorySelected)
   const [productsFiltered, setProductsFiltered] = useState([])
   const [keyword, setKeyword] = useState("")
 
-  const handlerKeyword = (k) => {
+   const handlerKeyword = (k) => {
     setKeyword(k)
   }
 
   useEffect(() => {
-    if (categorySelected) setProductsFiltered(products.filter(product => product.category === categorySelected))
-    if (keyword) setProductsFiltered(productsFiltered.filter(product => {
+    setProductsFiltered(products)
+    if (keyword) setProductsFiltered(products.filter(product => {
       const productTitleLower = product.title.toLowerCase()
       const keywordLower = keyword.toLowerCase()
       return productTitleLower.includes(keywordLower)
     }))
-  }, [categorySelected, keyword])
+  }, [categorySelected, keyword, products])
 
 
   return (
